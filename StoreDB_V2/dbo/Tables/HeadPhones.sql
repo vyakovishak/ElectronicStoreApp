2c4faa833d109497dbfddf5094fe36624cc233b2
V <vyakovishak@gmail.com>
2022-06-20 01:32:01 -0400

    Add new DB

---
+++ StoreDB_V2/dbo/Tables/HeadPhones.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[HeadPhones] (
+    [index]                 BIGINT     NULL,
+    [customerReviewAverage] FLOAT (53) NULL,
+    [customerReviewCount]   FLOAT (53) NULL,
+    [image]                 TEXT       NULL,
+    [longDescription]       TEXT       NULL,
+    [manufacturer]          TEXT       NULL,
+    [name]                  TEXT       NULL,
+    [sku]                   BIGINT     NULL
+);
+
