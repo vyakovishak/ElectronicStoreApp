85fb04b23e615c4e80ad7c63498aad5fecdaedda
V <vyakovishak@gmail.com>
2022-06-20 01:30:10 -0400

    Add db again

---
+++ StoreDB/dbo/Tables/Customer.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [Username]  VARCHAR (50)  NOT NULL,
+    [Password]  VARCHAR (50)  NOT NULL,
+    [LastName]  VARCHAR (50)  NOT NULL,
+    [FirstName] VARCHAR (50)  NOT NULL,
+    [Address]   VARCHAR (250) NOT NULL,
+    [State]     VARCHAR (50)  NOT NULL,
+    [Zip]       INT           NOT NULL,
+    [Cart]      VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([Username] ASC)
+);
+
